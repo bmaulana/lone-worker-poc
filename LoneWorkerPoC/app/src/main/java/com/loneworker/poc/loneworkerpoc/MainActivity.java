83db9d45f02ab625ce7175ed4aea857232f93992
bmaulana <bmaulana273@gmail.com>
2015-12-08 14:16:43 +0000

    Initial project setup

---
+++ LoneWorkerPoC/app/src/main/java/com/loneworker/poc/loneworkerpoc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.loneworker.poc.loneworkerpoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
